9b100bf6757b325364f4a75b5e39733217366677
nelsonlarios <nelson.larios.developer@gmail.com>
2024-12-30 18:26:18 +0000

    fixing building error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -103,8 +103,8 @@ export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
-    return { message: "Deleted Invoice." };
+    //return { message: "Deleted Invoice." };
   } catch {
-    return { message: "Database Error: Failed to Delete Invoice." };
+    //return { message: "Database Error: Failed to Delete Invoice." };
   }
 }
